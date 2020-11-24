a9ee81fc78945f24f6dcddcffa77da706911aa25
neurosenya <mrsena1206@gmail.com>
2020-11-24 15:33:11 +0200

    added plots for peaks and limits of integration

--- ContractionAnalysis.py
+++ ContractionAnalysis.py
@@ -19,6 +19,7 @@ from itertools import cycle
 import peakutils
 from scipy.signal import find_peaks, peak_widths
 from scipy.integrate import simps
+from BaselineRemoval import BaselineRemoval
 plt.style.use('dark_background')
 
 color=iter(cycle(plt.cm.rainbow(np.linspace(0,1,len(substances)))))
@@ -54,7 +55,46 @@ axs[1].set_title('After the alignment procedure')
 axs[1].set(xlabel = 'time [min]')
 
 fig.savefig(experiment_name + '/General_traces.pdf')
+
+# Finding variables required for calculating amplitude, areas under peaks, half-widths
+
+aligned = BaselineRemoval(contrac_aligned)
+zhang = aligned.ZhangFit()
+
+peaks, peak_values = find_peaks(zhang, height=0.5, distance=200)
+widths, widths_heights, left, right = peak_widths(zhang, peaks, rel_height=0.98)
+left = left.astype(int)
+right = right.astype(int)
+left_full = left * Δt
+right_full = right * Δt
+
+half_widths, widths_heights_half, left_half, right_half  = peak_widths(contrac_aligned, peaks, rel_height=0.5)
+
+fig, axs = plt.subplots(2, 1, figsize=(20, 8), dpi=130)
+axs[0].plot(time, contrac_aligned)
+axs[0].plot(time[peaks], contrac_aligned[peaks], 'x')
+
+axs[1].plot(time, contrac_aligned)
+axs[1].hlines(widths_heights_half, left_half*Δt, right_half*Δt, color='red')
+axs[1].set(xlabel='time [min]')
+axs[1].set_xlim([0, 30])
+
+fig.savefig(experiment_name + '/Peaks_half_widths_amplitude.pdf')
 			
-			
-			
-			
+# plotting limits of integratiobn
+fig, axs = plt.subplots(len(substances)+1, 1, 
+						figsize=(20, 4*(len(substances))+1), dpi=130);
+
+applications = list(substances.values())
+applicants_names = list(substances.keys())
+for ax, times, substance in zip(axs, 
+								 [(0, applications[0][0]-1)] + applications,
+								 ['control'] + applicants_names):
+	ax.plot(time, contrac_aligned, label=substance)
+	ax.legend()
+	ax.hlines(widths_heights, left_full, right_full, color='red')
+	ax.set_xlim(list(times))
+
+axs[-1].set(xlabel='time [min]')
+
+fig.savefig(experiment_name + '/Limits_of_integration.pdf')
