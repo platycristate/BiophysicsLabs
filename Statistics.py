f1d63e3eba4735eb39d768119984df3ecba0bcf1
neurosenya <mrsena1206@gmail.com>
2020-12-01 22:08:58 +0200

    Bonferoni correction, Shapiro-Wilco test, Mann-Whitney test

--- Statistics.py
+++ Statistics.py
@@ -1,13 +1,14 @@
-# threshold p-value for t_test
+# threshold p-value for t_test or U-test
 thresh = 0.05
 
 
 # Calculates significance of difference between groups
 import pickle
 import matplotlib.pyplot as plt
+import random
 import numpy as np
 import pandas as pd
-from scipy.stats import shapiro, ttest_rel
+from scipy.stats import shapiro, ttest_rel, mannwhitneyu
 plt.style.use('dark_background')
 
 experiment_name = 'Task2'
@@ -42,13 +43,34 @@ plt.savefig(experiment_name + '/Stats/histograms_hws.pdf')
 keys = list(amplitudes_sep.keys())
 vals = amplitudes_sep.values()
 p_values = []
+
+# t-test requires the same amount of points
+# for each sample =(
 mv = min([len(v) for v in vals])
+# tests for normal distribution
+# make random samples of length of the smallest group
+amplitudes_sep = {key: random.sample(val, mv) for key, val in amplitudes_sep.items()}
+shapiro_res = {key: shapiro(val)[1] for key, val in amplitudes_sep.items()}
+
 for i in range(len(keys)):
     for j in range(i+1, len(keys)):
-        p_values.append( (str(keys[i]), str(keys[j]), ttest_rel(amplitudes_sep[keys[i]][:mv], amplitudes_sep[keys[j]][:mv])[1]))
+        if shapiro_res[keys[i]] > thresh and shapiro_res[keys[j]] > thresh:
+            p_values.append( (str(keys[i]), str(keys[j]), ttest_rel(amplitudes_sep[keys[i]][:mv], amplitudes_sep[keys[j]][:mv])[1]) )
+        else:
+            # Mann-Whitney rank test
+            p_values.append( (str(keys[i]), str(keys[j]), mannwhitneyu(amplitudes_sep[keys[i]][:mv], amplitudes_sep[keys[j]][:mv])[1]) )
+		
+# Bonferoni correction	
+thresh /= len(p_values) # divide by the number of comparisons
 
+# Find pairs with significant difference in their means
 pval_df = pd.DataFrame(columns=keys, index=keys)
 for i in p_values:
 	if i[2] < thresh:
-		pval_df.loc[i[0], i[1]] = '<' + str(thresh)
+		pval_df.loc[i[0], i[1]] = '<' + str(round(thresh, 3))
 pval_df.to_csv(experiment_name + '/Stats/t_tests_pvals.csv')
+
+# saving results of shapiro test
+print(shapiro_res)
+with open(experiment_name + '/Stats/shapiro_res.p', 'wb') as f:
+	pickle.dump(shapiro_res, f)
